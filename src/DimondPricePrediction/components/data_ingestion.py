e058c50df8e0a76462f6816063fdd67bb18a8a08
NandiniMusale <vijayanandinimusale@gmail.com>
2023-11-04 14:17:09 +0530

    data is added

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,5 +1,22 @@
+import pandas as pd
+import numpy as np
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exception import customexception
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import path
+
+
+
+
+
 class DataIngestion:
     def __init__(self):
         pass
     def initialize_data_ingestion(self):
-        pass
+        logging.info("data_ingestion started")
+
+        try:
+            pd.read_csv(path(os.path.join("notebooks/data","gemstone.csv")))
+        except Exception as e:
+            pass
